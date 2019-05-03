888530d204cc32c9e3f859ca2f10b915d4f8723f
juan pernumian <juan.pernumian@mercadolibre.com>
2019-05-03 01:44:36 -0300

    implement nodemailer for sending mails

--- services/sendMail.js
+++ services/sendMail.js
@@ -15,15 +15,14 @@ class mailService {
    */
   static sendMail(name, email, phone, expertise, message) {  
     const smtpConfig = {
-      host: 'tapir.avnam.net',
+      host: 'mail.bilog.com.ar',
       port: 465,
       secure: true, // use SSL
       auth: {
         user: 'juan.pernumian@bilog.com.ar',
         pass: 'Artemis1404'
       },
-      tls: {rejectUnauthorized: false},
-      debug:true
+      tls: { rejectUnauthorized: false }
     };
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport(smtpConfig);
@@ -31,7 +30,7 @@ class mailService {
     // setup e-mail data with unicode symbols
     const mailOptions = {
       from: `${name} || <${email}>`, // sender address
-      to: 'juan.pernumian@gmail.com', // list of receivers
+      to: 'juan.pernumian@gmail.com, aldo.pernumian@gmail.com', // list of receivers
       subject: 'Contacto web', // Subject line
       text: `
         Nombre: ${name}
