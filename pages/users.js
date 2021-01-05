96436134a67c32f22b07db7f6c6ac5df4135993b
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-01-05 16:26:43 -0300

    Nueva pagina /users

---
+++ pages/users.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import Cover from '../components/Cover/Cover';
+
+export default () => (
+  <Layout>
+    <Cover
+      text="¡Bienvenido al centro de Usarios!"
+      paragraph="Aqui podras encontrar documentación, preguntas mas frecuentes, testimonios y como ponerte en contacto."
+      hasButtons={false}
+    />
+  </Layout>
+);
