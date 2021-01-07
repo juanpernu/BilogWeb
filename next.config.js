9cdf37c791deb8ca22af537aa06e43f916986c56
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-01-07 11:19:43 -0300

    Added the page users/faqs and set the route at next.config for it.

--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@ module.exports = {
       '/versions-detail/index': { page: 'versions-detail' },
       '/descarga/index': { page: 'download-go' },
       '/users/index': { page: 'users'},
+      '/users/faqs': { page: 'faqs'},
     }
   },
 }
---
+++ pages/faqs.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import Cover from '../components/Cover/Cover';
+
+export default () => (
+  <Layout>
+    <Cover
+      text="Preguntas frecuentes"
+      paragraph="Te dejamos a mano las preguntas más frecuentes con sus respectivas respuestas. En caso de no encontrar la pregunta o respuesta que estás buscando, no dudes en llamarnos!"
+      hasButtons={false}
+    />
+  </Layout>
+);
