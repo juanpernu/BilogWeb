84c4b93779871b336ac79455e200fbc3a23a2a70
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-01-08 13:34:17 -0300

    Created a JSON object that exports an array of questions and answers.

---
+++ mocks/faqs.js
@@ -0,0 +1,34 @@
+const faqs = {
+    faqs: [
+        {
+            question: "¿Es difi­cil la instalación del Software?",
+            answer: "Se realiza en forma sencilla, y en minutos usted tendrá su Software de Gestión Odontológica para Consultorios y Clí­nicas funcionando en su consultorio."
+        },
+        {
+            question: "¿Qué tipo de Sistema Operativo necesito?",
+            answer: "Necesita que su PC o notebook tenga Windows XP, Windows VISTA, Windows 7 o Windows 8."
+        },
+        {
+            question: "¿Necesito conexión a Internet?",
+            answer: "Sí­, es necesario que tenga conexión a Internet para utilizar nuestro Software de Gestión Odontológica para Consultorios y Clí­nicas. Le sugerimos banda ancha como mí­nimo 3 MB. Esto le servirá para el soporte técnico que realizamos por Team Viewer, para descargar actualizaciones desde el sistema, para el envío de SMS a pacientes, para la base de datos on line con los celulares o tablets y la facturación electrónica ente otras cosas."
+        },
+        {
+            question: "¿Cuánto demoro en tener el software funcionando?",
+            answer: "El software se descarga y se instala por internet entrega dentro de las 24 hs de haber recibido la confirmación de la compra. Si lo desea puede copiar los instaladores en un cd o pen drive, pero los mismos siempre se encuentran siempre disponibles en el link de descarga. La instalación demora aproximadamente 30 minutos."
+        },
+        {
+            question: "¿Que soporte técnico tendré luego de la compra del programa?",
+            answer: "Durante 60 dí­as la post venta es gratuita, agendamos capacitaciones ví­a internet y dictamos clases on line para poner en marcha el software. Luego si se desea pueden optar por el Servicio Plus que es nuestro soporte post-venta, que tiene un costo mí­nimo y cubre 100% las necesidades surgidas del uso del sistema, incluso actualizaciones y resguardo de los datos en un servidor on line."
+        },
+        {
+            question: "¿Las actualizaciones son gratis?",
+            answer: "Si estás adherido a nuestro Servicio Plus, las actualizaciones son totalmente gratuitas y las descargas desde el sistema. Si no, las que incluyen mejoras de funciones existentes son gratuitas. Las nuevas versiones tienen un costo accesible para que todos los usuarios dispongan de la última versión del software. Las actualizaciones se publican en nuestra página Web."
+        },
+        {
+            question: "¿Si adquiero la versión Small, puedo en el futuro cambiar de versión sin perder los datos?",
+            answer: "Sí­, puede cambiarse de versión sin perder ningún dato cargado anteriormente y con un solo click desde el mismo sistema y abonando la diferencia."
+        }
+    ]
+}
+
+module.exports = { faqs }
