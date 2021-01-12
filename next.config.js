a334c915668aa5f6c1f7daa6688c960e46443616
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-01-12 15:21:10 -0300

    Agregando la sub-seccion/vista llamada docuemntacion dentro de la seccion usuarios

--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@ module.exports = {
       '/versions-detail/index': { page: 'versions-detail' },
       '/descarga/index': { page: 'download-go' },
       '/users/index': { page: 'users'},
+      '/users/document':{page: 'document'}
     }
   },
 }
