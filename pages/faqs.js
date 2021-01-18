917b8376d82c65fa3192b9579bf67294a20f4664
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-01-18 14:12:01 -0300

    Create an accordion component that reads data from the JSON and renders it (BILW-009) (#28)
    * Created a Accordion and FAQ component.
    
    * feat: add hook to open nand close faq
    
    * Tweaked some styles.
    
    * Fixed the styles mentioned in the comments.
    
    * Changed a style from a comment I missed.
    
    Co-authored-by: Juan Manuel Pernumian <juan.pernumian@gmail.com>

--- pages/faqs.js
+++ pages/faqs.js
@@ -1,5 +1,8 @@
 import Layout from '../components/Layout';
 import Cover from '../components/Cover/Cover';
+import Accordion from '../components/Faq';
+
+import { faqs } from "../mocks/faqs";
 
 export default () => (
   <Layout>
@@ -8,5 +11,6 @@ export default () => (
       paragraph="Te dejamos a mano las preguntas más frecuentes con sus respectivas respuestas. En caso de no encontrar la pregunta o respuesta que estás buscando, no dudes en llamarnos!"
       hasButtons={false}
     />
+    <Accordion data={faqs} />
   </Layout>
 );
