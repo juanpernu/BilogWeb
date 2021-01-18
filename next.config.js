9986a5f9c50dc111faa5144b2c8346462593a95b
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-01-18 14:26:56 -0300

    Cambios en la ruta de la subseccion/vista docuemnts

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = {
       '/versions-detail/index': { page: 'versions-detail' },
       '/descarga/index': { page: 'download-go' },
       '/users/index': { page: 'users'},
-      '/users/document':{page: 'document'}
+      '/document':{page: 'document'}
     }
   },
 }
