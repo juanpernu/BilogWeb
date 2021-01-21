5aaabfa9a0d4e359c252a313a920b97495d7f6c8
Juan Pernumian <juan.pernumian@gmail.com>
2021-01-21 13:02:15 -0300

    Add testimonials mocks (#38)
    * Created a JSON Structure that contains the testimonials' information.
    
    * Deleted some unnecessary commas.
    
    * feat: remove unused export
    
    Co-authored-by: DidierSoules <36740732+DidierSoules@users.noreply.github.com>

---
+++ mocks/testimonials.js
@@ -0,0 +1,207 @@
+module.exports = {
+    testimonials: [
+      {
+        comment: `"Estoy Feliz con mi software, tiene todo lo que necesito para mi consultorio, además lo mas importante, el servicio técnico excelente!!!."`,
+        clinic: "Clinica Odontológica Vitaclinica",
+        location: "Asunción, Paraguay.",
+        users: [
+          {
+            name: "Dra. Ana Campuzano",
+          }
+        ]
+      },
+      {
+        comment: `"El sistema es bárbaro ya que me cuesta bastante tener en orden el papelerнo. Me ayudó a tener todo bien organizado. Tengo todo guardado en el sistema!"`,
+        clinic: "Consultorio Odontológico",
+        location: "San Fernando del Valle de Catamarca, Catamarca.",
+        users: [
+          {
+            name: "Dr. César Aguirre",
+          }
+        ]
+      },
+      {
+        comment: `"El software está funcionando muy bien, me ha sacado mucho papelerнo del escritorio y tengo todo mucho más organizado."`,
+        clinic: "Consultorio Odontológico",
+        location: "Capital Federal.",
+        users: [
+          {
+            name: "Dr. Jorge Aguilar",
+          }
+        ]
+      },
+      {
+        comment: `"El software funciona excelente y es: ágil, dinámico y práctico. Todo es muy bueno, la post venta y el soporte técnico."`,
+        clinic: "Consultorio Odontológico",
+        location: "San Nicolás, Buenos Aires.",
+        users: [
+          {
+            name: "Dr. Roman Abramor",
+          }
+        ]
+      },
+      {
+        comment: `"Estamos muy contentos. Todo es excelente, el producto y la atención post venta y el soporte técnico también es bárbaro."`,
+        clinic: "Consultorio Odontológico",
+        location: "San Isidro, Buenos Aires.",
+        users: [
+          {
+            name: "Dra. Carolina Leoni",
+          },
+          {
+            name: "Dr. Gonzalo Rivera",
+          }
+        ]
+      },
+      {
+        comment: `"Me gustaría decir que el Software de Gestión para Laboratorios de Bilog nos ha solucionado toda la parte administrativa del laboratorio. Estamos muy contentos y cumple con las expectativas que teníamos."`,
+        clinic: "Laboratorio Dental",
+        location: "Cerro las Rosas, Córdoba.",
+        users: [
+          {
+            name: "Dr. Eduardo Rodríguez Endrek",
+          }
+        ]
+      },
+      {
+        comment: `"El programa anda muy bien. Me agiliza todo el funcionamiento del consultorio."`,
+        clinic: "Consultorio Odontológico",
+        location: "Formosa, Capital.",
+        users: [
+          {
+            name: "Dra. Ana María Rodriguez Alonso",
+          }
+        ]
+      },
+      {
+        comment: `"Nunca he tenido un inconveniente con el software. Estoy muy conforme con el sistema, me ayudó a organizar el consultorio."`,
+        clinic: "Consultorio Odontológico",
+        location: "General Pico, La Pampa.",
+        users: [
+          {
+            name: "Dr. Juan Pablo Risi",
+          }
+        ]
+      },
+      {
+        comment: `"Utilizo mucho la parte de Ortodoncia del sistema. En cuanto a la atención es muy buena, y como me resulta muy fácil de usar no necesito soporte."`,
+        clinic: "Consultorio Odontológico",
+        location: "Chajarí, Entre Rнos.",
+        users: [
+          {
+            name: "Dra. Patricia Rios",
+          }
+        ]
+      },
+      {
+        comment: `"Estamos muy conformes con el programa. Todo funciona muy bien. Nos agilizó la organización del consultorio."`,
+        clinic: "Consultorio Odontológico",
+        location: "Capital Federal.",
+        users: [
+          {
+            name: "Dr. Darío Toucedo",
+          }
+        ]
+      },
+      {
+        comment: `"Estoy muy satisfecho con el software. Lo usa mi secretaria muy bien y nos agilizó las tareas administrativas del consultorio."`,
+        clinic: "Consultorio Odontológico",
+        location: "Tres Arroyos, Buenos Aires.",
+        users: [
+          {
+            name: "Dr. Alberto Tribiño",
+          }
+        ]
+      },
+      {
+        comment: `"Estoy muy conforme con el sistema."`,
+        clinic: "Consultorio Odontológico",
+        location: "El colorado, Formosa.",
+        users: [
+          {
+            name: "Dr. Ramón Rubén Ugartemendía",
+          }
+        ]
+      },
+      {
+        comment: `"Estamos muy conformes con el sistema y la atención. Todo funciona muy bien."`,
+        clinic: "Consultorio Odontológico",
+        location: "Castelar, Buenos Aires.",
+        users: [
+          {
+            name: "Dr. Luis Valenzuela",
+          }
+        ]
+      },
+      {
+        comment: `"El software me parece muy bueno. Estamos muy conformes."`,
+        clinic: "Consultorio Odontológico",
+        location: "Capital Federal.",
+        users: [
+          {
+            name: "Dr. Leandro Valeri",
+          }
+        ]
+      },
+      {
+        comment: `"Estoy muy conforme con el software. Todo muy bueno."`,
+        clinic: "Consultorio Odontológico",
+        location: "Sáenz Peña, Chaco.",
+        users: [
+          {
+            name: "Dra. Ileana Valussi",
+          }
+        ]
+      },
+      {
+        comment: `"El software es muy bueno."`,
+        clinic: "Consultorio Odontológico",
+        location: "Santa Rosa, La Pampa.",
+        users: [
+          {
+            name: "Dra. Maria Elena Veliz",
+          }
+        ]
+      },
+      {
+        comment: `"Destaco la excelencia en el servicio, la buena predisposición y el trato."`,
+        clinic: "Consultorio Odontológico",
+        location: "Santa Fe, Capital.",
+        users: [
+          {
+            name: "Dr. Matías Vento",
+          }
+        ]
+      },
+      {
+        comment: `"El software está muy bueno. Mejor a otros que he utilizado. Estoy totalmente conforme."`,
+        clinic: "Consultorio Odontológico",
+        location: "Gral. Alvear, Mendoza.",
+        users: [
+          {
+            name: "Dr. Sergio Viñolo",
+          }
+        ]
+      },
+      {
+        comment: `"Respecto al software está todo muy bien. Y el soporte y la capacitación ha sido excelente."`,
+        clinic: "Consultorio Odontológico",
+        location: "Capital Federal.",
+        users: [
+          {
+            name: "Dr. Claudio Vollmer",
+          }
+        ]
+      },
+      {
+        comment: `"El sistema me resultó muy bueno. Me ha dado muy buen resultado."`,
+        clinic: "Consultorio Odontológico",
+        location: "Laborde, Córdoba.",
+        users: [
+          {
+            name: "Dra. Maria José Williams",
+          }
+        ]
+      },
+    ]
+  };
