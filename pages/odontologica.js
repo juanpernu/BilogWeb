b28a7ece692e5ed31e80a3c9e3167100c92fc2f7
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-01-28 13:10:23 -0300

    Some fixes.

--- pages/odontologica.js
+++ pages/odontologica.js
@@ -1,24 +1,23 @@
 import Layout from '../components/Layout';
 import Cover from '../components/Cover/Cover';
-import BannerCta from '../components/Banner/BannerCta';
 import Feature from '../components/Feature/Feature';
 import Carousel from '../components/Carousel';
 import ProductSlider from '../components/Product/ProductSlider';
 import GeneratedVideos from '../components/Videos/Videos'
 import MockedFeatures from '../mocks/odontologicaFeatures';
-import  Data  from '../mocks/odontologicaMock';
+import  { productCover, appCover, features }  from '../mocks/odontologicaMock';
 
 export default () => (
     <Layout>
       <Cover
-        text={Data.productCover.text}
-        paragraph={Data.productCover.paragraph}
-        position={Data.productCover.position}
-        hasButtons={Data.productCover.hasButtons}
-        gradientBg={Data.productCover.gradientBg}
-        coverImage={Data.productCover.coverImage}
+        text={productCover.text}
+        paragraph={productCover.paragraph}
+        position={productCover.position}
+        hasButtons={productCover.hasButtons}
+        gradientBg={productCover.gradientBg}
+        coverImage={productCover.coverImage}
       />
-      <Feature {...MockedFeatures} />
+      <Feature features={features} />
       {/* 
         TODO: Update the video of this banner to render this component
         <BannerCta
@@ -29,14 +28,14 @@ export default () => (
       /> */}
       <ProductSlider />
       <Cover
-        text={Data.appCover.text}
-        paragraph={Data.appCover.paragraph}
-        hasButtons={Data.appCover.hasButtons}
-        buttonText={Data.appCover.buttonText}
-        buttonHref={Data.appCover.buttonHref}
-        position={Data.appCover.position}
-        gradientBg={Data.appCover.gradientBg}
-        coverImage={Data.appCover.coverImage}
+        text={appCover.text}
+        paragraph={appCover.paragraph}
+        hasButtons={appCover.hasButtons}
+        buttonText={appCover.buttonText}
+        buttonHref={appCover.buttonHref}
+        position={appCover.position}
+        gradientBg={appCover.gradientBg}
+        coverImage={appCover.coverImage}
       />
       <Carousel/>
     </Layout>
