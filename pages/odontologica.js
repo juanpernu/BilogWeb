8aef0b4ef655ebd9483453a88e5c319290944713
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-02-03 12:24:26 -0300

    Adjust component for the new layout (BILW-027) (#49)
    * Reworked the Feature component to be able to display vertically, horizontally, with or without image.
    
    * Fixes.
    
    * Fix.
    
    * Created /feature page to show the new layouts. Also created a new mock for it and left /odontologica back to the its original.
    
    * Made both pages export a constant.
    
    * Changed name of a constant.
    
    * Changed const name.

--- pages/odontologica.js
+++ pages/odontologica.js
@@ -4,20 +4,20 @@ import Feature from '../components/Feature/Feature';
 import Carousel from '../components/Carousel';
 import ProductSlider from '../components/Product/ProductSlider';
 import GeneratedVideos from '../components/Videos/Videos'
-import  { productCover, appCover, features }  from '../mocks/odontologicaMock';
+import { productCover, appCover, features } from '../mocks/odontologicaMock';
 
-export default () => (
-    <Layout>
-      <Cover
-        text={productCover.text}
-        paragraph={productCover.paragraph}
-        position={productCover.position}
-        hasButtons={productCover.hasButtons}
-        gradientBg={productCover.gradientBg}
-        coverImage={productCover.coverImage}
-      />
-      <Feature features={features} />
-      {/* 
+const Odontologica = () => (
+  <Layout>
+    <Cover
+      text={productCover.text}
+      paragraph={productCover.paragraph}
+      position={productCover.position}
+      hasButtons={productCover.hasButtons}
+      gradientBg={productCover.gradientBg}
+      coverImage={productCover.coverImage}
+    />
+    <Feature features={features} />
+    {/* 
         TODO: Update the video of this banner to render this component
         <BannerCta
         title="¿Todavía tenés dudas? Mirá la demo."
@@ -25,17 +25,19 @@ export default () => (
         modalContent={<GeneratedVideos type='intro'/>}
         showModal={true}
       /> */}
-      <ProductSlider />
-      <Cover
-        text={appCover.text}
-        paragraph={appCover.paragraph}
-        hasButtons={appCover.hasButtons}
-        buttonText={appCover.buttonText}
-        buttonHref={appCover.buttonHref}
-        position={appCover.position}
-        gradientBg={appCover.gradientBg}
-        coverImage={appCover.coverImage}
-      />
-      <Carousel/>
-    </Layout>
+    <ProductSlider />
+    <Cover
+      text={appCover.text}
+      paragraph={appCover.paragraph}
+      hasButtons={appCover.hasButtons}
+      buttonText={appCover.buttonText}
+      buttonHref={appCover.buttonHref}
+      position={appCover.position}
+      gradientBg={appCover.gradientBg}
+      coverImage={appCover.coverImage}
+    />
+    <Carousel />
+  </Layout>
 )
+
+export default Odontologica;
