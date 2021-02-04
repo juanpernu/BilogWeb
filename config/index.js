551f1bb851258207aabe6e55be06eca8c2f3d720
Juan Manuel Pernumian <juan.pernumian@gmail.com>
2021-02-04 16:06:37 -0300

    feat: add gtm for chile

---
+++ config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tagManagerConfig: {
+    AR: {
+      gtmId: 'GTM-WF5KDWF'
+    },
+    CL: {
+      gtmId: 'GTM-W8CZ5KX',
+    },
+  },
+  hotsjarConfig: {
+    hjid: 2135805,
+    hjsv: 6,
+  }
+};
