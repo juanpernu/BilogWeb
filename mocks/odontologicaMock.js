d0200dda2a7864cd230852ce816c94e46a10c738
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-02-05 11:07:44 -0300

    Added the component button to the FeatureItem component and adapted the mock for it (BILW-020) (#51)
    * Added the component button to the FeatureItem component and adapted the mock for it.
    
    * Some style changes.
    
    * Fixes.

--- mocks/odontologicaMock.js
+++ mocks/odontologicaMock.js
@@ -37,16 +37,28 @@ module.exports = {
           title: "Agenda de Turnos",
           description: "Agendá turnos en forma más eficiente. Los diferentes colores te van a ayudar a visibilizar tiempos muertos, sobreturnos, pacientes de primera vez, entre otras cosas más.",
           icon: 'agenda',
+          button: {
+            externalLink: "/users",
+            text: "Ver más",
+          },
         },
         {
           title: "Pacientes",
           description: "Bilog te ayuda con la carga de los datos personales de tus pacientes. Siempre vas a tener a la vista el saldo actual de tus pacientes, vas a poder agregar su foto, enviarle un mail sin salir del sistema o un mensaje.",
           icon: 'person',
+          button: {
+            externalLink: "/users",
+            text: "Ver más",
+          },
         },
         {
           title: "Administración",
           description: "Registrá los ingresos y egresos de forma más eficiente para maximizar la productividad de tu trabajo",
           icon: 'account',
+          button: {
+            externalLink: "/users",
+            text: "Ver más",
+          },
         }
       ],
     },
@@ -61,16 +73,28 @@ module.exports = {
           title: "Copia de Seguridad",
           description: "La seguridad de tus datos es lo más importante para nosotros. Por eso el sistema cuenta con la función de copias de seguridad automáticas.",
           icon: 'cloud',
+          button: {
+            externalLink: "/users",
+            text: "Ver más",
+          },
         },
         {
           title: "Estadísticas",
           description: "Llevá un registro de tus finanzas de una forma más eficiente para maximizar y optimizar la productividad de tu profesión.",
           icon: 'chart',
+          button: {
+            externalLink: "/users",
+            text: "Ver más",
+          },
         },
         {
           title: "Liquidaciones",
           description: "Liquidá a profesionales, obras sociales y laboratorios. Llevá el control de tus finanzas y de tu trabajo.",
           icon: 'money',
+          button: {
+            externalLink: "/users",
+            text: "Ver más",
+          },
         }
       ],
     }
