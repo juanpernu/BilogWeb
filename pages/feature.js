fd0d2b8203a791087e7cb8e5da5893475e7163c8
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-02-05 15:03:50 -0300

    Rendering feature information using context (incomplete).

--- pages/feature.js
+++ pages/feature.js
@@ -1,29 +1,14 @@
 import Layout from '../components/Layout';
-import Cover from '../components/Cover/Cover';
-import Feature from '../components/Feature/Feature';
-import { productCover, appCover, features } from '../mocks/featureTest';
+import FeatureProvider from '../contexts/featureContext';
+import FeatureWrapper from '../components/Feature/FeatureWrapper';
+
+const featureContent = require('../contents/feature/content');
 
 const Features = () => (
     <Layout>
-        <Cover
-            text={productCover.text}
-            paragraph={productCover.paragraph}
-            position={productCover.position}
-            hasButtons={productCover.hasButtons}
-            gradientBg={productCover.gradientBg}
-            coverImage={productCover.coverImage}
-        />
-        <Feature features={features} />
-        <Cover
-            text={appCover.text}
-            paragraph={appCover.paragraph}
-            hasButtons={appCover.hasButtons}
-            buttonText={appCover.buttonText}
-            buttonHref={appCover.buttonHref}
-            position={appCover.position}
-            gradientBg={appCover.gradientBg}
-            coverImage={appCover.coverImage}
-        />
+        <FeatureProvider data={featureContent}>
+            <FeatureWrapper />
+        </FeatureProvider>
     </Layout>
 )
 
