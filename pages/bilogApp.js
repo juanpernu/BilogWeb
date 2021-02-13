d0933248d370ae60bb4044dbd928060176393d1b
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-02-13 11:36:20 -0300

    Espacios innecesario eliminados.

--- pages/bilogApp.js
+++ pages/bilogApp.js
@@ -15,9 +15,7 @@ export default () => (
       coverImage={productCover.coverImage}
       appButtons
     />
-
     <Feature features={features} />
-
     <FeatureImg
       imgSrc={featureImg.imgSrc}
       imgAlt={featureImg.imgAlt}
