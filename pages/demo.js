48feb9b3666bf7849a0644f2287436cc11851a05
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-03-02 16:08:30 -0300

    New demo page

---
+++ pages/demo.js
@@ -0,0 +1,15 @@
+import Cover from "../components/Cover/Cover";
+import Layout from "../components/Layout";
+
+const Demo = ()=>{
+    return(
+        <Layout>
+            <Cover
+                text="Bienvenidos a la demo Bilog!"
+                paragraph="Observa la demostración guiada de Bilog y descubre por qué somos tu mejor opción como software dental." 
+                hasButtons={false}/>
+        </Layout>
+    )
+}
+
+export default Demo;
