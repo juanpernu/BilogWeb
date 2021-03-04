7355650e1552a7516778ba7219a7da3bd4eeff1a
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-03-04 15:07:14 -0300

    Replicate component Features found in odontologica

--- pages/demo.js
+++ pages/demo.js
@@ -1,5 +1,7 @@
 import Cover from "../components/Cover/Cover";
 import Layout from "../components/Layout";
+import Feature from "../components/Feature/Feature"
+import MockedFeatures from '../mocks/odontologicaFeatures'
 
 const Demo = ()=>{
     return(
@@ -7,6 +9,7 @@ const Demo = ()=>{
             <Cover
                 text="Bienvenidos a la demo Bilog!"
                 paragraph="Observa la demostración guiada de Bilog y descubre por qué somos tu mejor opción como software dental."/>
+            <Feature {...MockedFeatures} />
         </Layout>
     )
 }
