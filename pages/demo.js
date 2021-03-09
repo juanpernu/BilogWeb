c1a3d56039a0181b261f73d1fc6eb2e817892ee4
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-03-09 08:44:51 -0300

    Changes in the title of the page /demo

--- pages/demo.js
+++ pages/demo.js
@@ -6,7 +6,7 @@ const Demo = ()=>{
         <Layout>
             <Cover
                 text="Bienvenidos a la demo Bilog!"
-                paragraph="Observa la demostración guiada de Bilog y descubre por qué somos tu mejor opción como software dental."
+                paragraph="Mirá la demostración guiada de nuestro software y descubrí por qué somos tu mejor opción como software dental."
                 coverVideo="https://www.youtube.com/embed/aLe4pjyI4bA?start=4"/>
         </Layout>
     )
