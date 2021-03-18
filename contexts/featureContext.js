fa38891ac8e3c5b4243684587115f2bfd73f94a5
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-03-18 16:34:11 -0300

    Dump content of each function in its respective product page and modifications in items feature texts in /odontogica

--- contexts/featureContext.js
+++ contexts/featureContext.js
@@ -1,29 +1,27 @@
 import React from 'react';
-
 export const FeatureContext = React.createContext();
-
 class FeatureProvider extends React.Component {
     constructor(props) {
         super(props);
         const { data, hash = '#agenda' } = this.props;
         this.state = {
             productCover: data[hash].productCover,
-            appCover: data[hash].appCover,
+            banner: data[hash].banner,
             features: data[hash].features,
+            appCover: data[hash].appCover,
             allContent: (contentId) => this.getFeatureContent(contentId),
         };
         this.getFeatureContent = this.getFeatureContent.bind(this);
     }
-
     getFeatureContent(contentId) {
         const { data } = this.props;
         this.setState({
             productCover: data[contentId].productCover,
+            banner: data[contentId].banner, 
             appCover: data[contentId].appCover,
             features: data[contentId].features,
         });
     };
-
     render() {
         return (<FeatureContext.Provider
             key="provider"
@@ -36,5 +34,4 @@ class FeatureProvider extends React.Component {
         </FeatureContext.Provider>);
     };
 }
-
 export default FeatureProvider;
