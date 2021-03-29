33965cf154faff61b4906830f0ba0cc7666798ef
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-03-29 19:11:57 -0300

    Deleted api and services folder.

--- pages/odontologica.js
+++ pages/odontologica.js
@@ -9,17 +9,17 @@ import GeneratedVideos from '../components/Videos/Videos'
 import MockedFeatures from '../mocks/odontologicaFeatures';
 
 export default () => (
-    <Layout>
-      <Cover
-        text="El software que te merecés."
-        paragraph="Tené en una sola ventana todo el control de tu clínica para una mejor gestión."
-        position="lf"
-        hasButtons={false}
-        gradientBg="default"
-        coverImage="odontologica"
-      />
-      <Feature {...MockedFeatures} />
-      {/* 
+  <Layout>
+    <Cover
+      text="El software que te merecés."
+      paragraph="Tené en una sola ventana todo el control de tu clínica para una mejor gestión."
+      position="lf"
+      hasButtons={false}
+      gradientBg="default"
+      coverImage="odontologica"
+    />
+    <Feature {...MockedFeatures} />
+    {/* 
         TODO: Update the video of this banner to render this component
         <BannerCta
         title="¿Todavía tenés dudas? Mirá la demo."
@@ -27,18 +27,18 @@ export default () => (
         modalContent={<GeneratedVideos type='intro'/>}
         showModal={true}
       /> */}
-      <ProductSlider />
-      <Cover
-        text="Te presentamos Bilog App"
-        paragraph="Tu consultorio, a donde sea que vayas."
-        hasButtons={true}
-        buttonText="Ver más"
-        buttonHref="/bilogApp"
-        product={true}
-        position="lf"
-        gradientBg="default"
-        coverImage="app"
-      />
-      <Carousel/>
-    </Layout>
+    <ProductSlider />
+    <Cover
+      text="Te presentamos Bilog App"
+      paragraph="Tu consultorio, a donde sea que vayas."
+      hasButtons={true}
+      buttonText="Ver más"
+      buttonHref="/bilogApp"
+      product={true}
+      position="lf"
+      gradientBg="default"
+      coverImage="app"
+    />
+    <Carousel />
+  </Layout>
 )
