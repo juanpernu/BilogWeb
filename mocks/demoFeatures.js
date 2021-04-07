874322820a74d7d40e6d711ab0b2e9ca61c782ff
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-04-07 16:31:39 -0300

    Changed the title and copy of features and created a mock just for demoFeatures.

---
+++ mocks/demoFeatures.js
@@ -0,0 +1,52 @@
+module.exports = {
+    features: [
+        {
+            title: "Conocé las funciones que te van a ayudar a impulsar tu negocio",
+            description: "Te van a ayudar a mejorar tu consultorio en muy poco tiempo.",
+            successCase: {
+                text: "Mira cómo la Dra. Calcagno mejoró el presentismo de sus consultorios",
+                href: "/success-calcagno",
+            },
+            hasColorBackground: false,
+            featureItems: [
+                {
+                    title: "Agenda de Turnos",
+                    description: "Agendá turnos en forma más eficiente. Los diferentes colores te van a ayudar a visibilizar tiempos muertos, sobreturnos, pacientes de primera vez, entre otras cosas más.",
+                    icon: 'agenda',
+                },
+                {
+                    title: "Pacientes",
+                    description: "Bilog te ayuda con la carga de los datos personales de tus pacientes. Siempre vas a tener a la vista el saldo actual de tus pacientes, vas a poder agregar su foto, enviarle un mail sin salir del sistema o un mensaje.",
+                    icon: 'person',
+                },
+                {
+                    title: "Administración",
+                    description: "Registrá los ingresos y egresos de forma más eficiente para maximizar la productividad de tu trabajo",
+                    icon: 'account',
+                }
+            ],
+        },
+        {
+            title: "Maximizá la productividad de tu consultorio o clínica",
+            description: "Con Bilog podes registrar los ingresos y egresos en forma más eficiente y así maximizar la rentabilidad.",
+            hasColorBackground: true,
+            featureItems: [
+                {
+                    title: "Copia de Seguridad",
+                    description: "La seguridad de tus datos es lo más importante para nosotros. Por eso el sistema cuenta con la función de copias de seguridad automáticas.",
+                    icon: 'cloud',
+                },
+                {
+                    title: "Estadísticas",
+                    description: "Llevá un registro de tus finanzas de una forma más eficiente para maximizar y optimizar la productividad de tu profesión.",
+                    icon: 'chart',
+                },
+                {
+                    title: "Liquidaciones",
+                    description: "Liquidá a profesionales, obras sociales y laboratorios. Llevá el control de tus finanzas y de tu trabajo.",
+                    icon: 'money',
+                }
+            ],
+        }
+    ]
+};
--- pages/demo.js
+++ pages/demo.js
@@ -1,23 +1,23 @@
 import Layout from "../components/Layout";
 import Cover from "../components/Cover/Cover";
 import Feature from "../components/Feature/Feature"
-import MockedFeatures from '../mocks/odontologicaFeatures'
+import MockedFeatures from '../mocks/demoFeatures'
 import BannerCta from "../components/Banner/BannerCta";
 import Carousel from '../components/Carousel';
 
-const Demo = ()=>{
-    return(
+const Demo = () => {
+    return (
         <Layout>
             <Cover
                 text="Bienvenidos a la demo Bilog!"
                 paragraph="Mirá la demostración guiada de nuestro software y descubrí por qué somos tu mejor opción como software dental."
-                coverVideo="https://www.youtube.com/embed/aLe4pjyI4bA?start=4"/>
+                coverVideo="https://www.youtube.com/embed/aLe4pjyI4bA?start=4" />
             <Feature {...MockedFeatures} />
             <BannerCta
                 title="¿Todavia tenés dudas? Contactanos."
                 buttonText="Ir a contactos"
                 buttonHref="/contact" />
-            <Carousel/>    
+            <Carousel />
         </Layout>
     )
 }
