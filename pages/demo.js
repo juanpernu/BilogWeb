999a3e1f63d03e6b3dce65748e8b8da480d34c38
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-04-07 16:17:04 -0300

    Changed the title and copy of the cover at /demo

--- pages/demo.js
+++ pages/demo.js
@@ -5,19 +5,19 @@ import MockedFeatures from '../mocks/odontologicaFeatures'
 import BannerCta from "../components/Banner/BannerCta";
 import Carousel from '../components/Carousel';
 
-const Demo = ()=>{
-    return(
+const Demo = () => {
+    return (
         <Layout>
             <Cover
-                text="Bienvenidos a la demo Bilog!"
-                paragraph="Mirá la demostración guiada de nuestro software y descubrí por qué somos tu mejor opción como software dental."
-                coverVideo="https://www.youtube.com/embed/aLe4pjyI4bA?start=4"/>
+                text="Demostración de Bilog"
+                paragraph="Mirá la demostración guiada de nuestro software y descubrí por qué somos la mejor opción."
+                coverVideo="https://www.youtube.com/embed/aLe4pjyI4bA?start=4" />
             <Feature {...MockedFeatures} />
             <BannerCta
                 title="¿Todavia tenés dudas? Contactanos."
                 buttonText="Ir a contactos"
                 buttonHref="/contact" />
-            <Carousel/>    
+            <Carousel />
         </Layout>
     )
 }
