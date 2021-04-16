f54c27c42b8a5afbd08177ea7b5e3acdb4809c8d
Juan Manuel Pernumian <juan.pernumian@gmail.com>
2021-04-16 15:42:55 -0300

    fix: fix styles import problem

--- components/Case/index.js
+++ components/Case/index.js
@@ -1,23 +1,23 @@
 import SuccessImg from '../Success/Image';
 import ButtonLink from '../Buttons/ButtonLink';
 
-import './index.module.scss';
+import styles from './index.module.scss';
 
 const Case = props => {
   const { cases } = props;
   return cases.map(c => {
     return (
-      <div className="success-case">
+      <div className={styles["success-case"]}>
         <SuccessImg imgBg={c.cover.src} bgPosition={c.cover.position} />
-        <p className="success-case_copy">Caso de {c.case}</p>
-        <h2 className="success-case_title">{c.title}</h2>
+        <p className={styles["success-case_copy"]}>Caso de {c.case}</p>
+        <h2 className={styles["success-case_title"]}>{c.title}</h2>
         <p>{c.preview}</p>
         <ButtonLink
           customClass="terciary"
           buttonHref={c.href}
           buttonText="Leer más"
         />
-        {!c.no_divider && <div className="success-case_divider" />}
+        {!c.no_divider && <div className={styles["success-case_divider"]} />}
       </div>
     )
   })
