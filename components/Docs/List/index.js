764e00bec6b6bd5ade54cf20dd130c4ce7b410d4
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-07-13 17:47:21 -0300

    Created the page /privacy

--- components/Docs/List/index.js
+++ components/Docs/List/index.js
@@ -1,17 +1,30 @@
 import styled from './index.module.scss';
-const List = ({title, content}) =>{
-  return(
-      <div className={styled["list"]}>
-        <h3 className={styled["list-title"]}>{title}</h3>
-        <ul className={styled["list-container"]}>
-          {content.map((el, index)=>{
-            const {text} = el 
-            return(
-              <li key={index}>{text}</li>
-            )
-          })}
-        </ul>
-      </div>
-    ) 
+
+const List = ({ title, content }) => {
+  return (
+    <div className={styled["list"]}>
+      <h3 className={styled["list-title"]}>{title}</h3>
+      <ul className={styled["list-container"]}>
+        {content.map((el, index) => {
+          const { text, link, hasLink } = el
+          return (
+            hasLink ?
+              <li key={index}>
+                <a className={styled["link"]} href={link}>
+                  {text}
+                </a>
+              </li>
+              :
+              <>
+                <li key={index}>
+                  {text}
+                </li>
+              </>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
+
 export default List;
